813b634f6e270776853c06af383ee2bf9973ee3c
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2020-12-14 12:54:00 +0000

    staged all files

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,4 +1,4 @@
-async function Game(canvas){
+async function Game(canvas) {
     const game = this;
 
     game.canvas = canvas;
@@ -13,7 +13,7 @@ async function Game(canvas){
 
 }
 
-async function createBackground(canvas){
+async function createBackground(canvas) {
     const background = new Background(canvas)
     await background.Draw()
 
@@ -28,13 +28,13 @@ async function getFences() {
     }).then(function (JSONObject) {
         //console.log(JSONObject)
         fenceObject = JSONObject
-    }).catch(function (error){
+    }).catch(function (error) {
         console.log('Data failed to load')
         console.log(error)
     })
 
     const Fences = []
-    for (let i =0; i < fenceObject.Fences.length; i++) {
+    for (let i = 0; i < fenceObject.Fences.length; i++) {
         let fence = new FenceFactory(canvas)
         fence.x = fenceObject.Fences[i].x
         fence.y = fenceObject.Fences[i].y
@@ -46,27 +46,28 @@ async function getFences() {
 
 
 }
-async function createMaze(canvas,fences){
-    for (let i = 0; i < fences.length; i++){
-        fences[i].DrawTile(fences[i].src, fences[i].x,fences[i].y)
+
+async function createMaze(canvas, fences) {
+    for (let i = 0; i < fences.length; i++) {
+        fences[i].DrawTile(fences[i].src, fences[i].x, fences[i].y)
     }
 }
 
 async function GameEngine(canvas, context, fences, background, sheep) {
     window.requestAnimationFrame(animationLoop)
 
-    let counter =0;
+    let counter = 0;
     let start = new Date()
 
     function animationLoop() {
 
         let now = new Date()
-        if (now - start >= 100){
-            start =now
-            context.clearRect(0,0, canvas.width, canvas.height)
+        if (now - start >= 100) {
+            start = now
+            context.clearRect(0, 0, canvas.width, canvas.height)
 
             counter++
-            counter %=6
+            counter %= 6
 
             background.DrawTile()
             createMaze(canvas, fences)
@@ -75,5 +76,27 @@ async function GameEngine(canvas, context, fences, background, sheep) {
         window.requestAnimationFrame(animationLoop)
 
     }
-    
+
+    window.addEventListener("keydown", function (event) {
+        if (event.code === "ArrowUp") {
+            console.log(event.code)
+            sheep.y = sheep.y-5
+        }
+        if (event.code === "ArrowDown") {
+            console.log(event.code)
+            sheep.y = sheep.y+5
+
+        }
+        if (event.code === "ArrowRight") {
+            console.log(event.code)
+            sheep.x = sheep.x +5
+
+        }
+        if (event.code === "ArrowLeft") {
+            console.log(event.code)
+            sheep.x = sheep.x -5
+
+        }
+    })
+
 }
