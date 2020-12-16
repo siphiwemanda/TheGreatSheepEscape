6b2c706762e22d7f8751b294af7fba534b19ced6
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2020-12-16 21:24:29 +0000

    refactor
    End screen now shows when sheep has no more lives

--- src/js/Lives.js
+++ src/js/Lives.js
@@ -3,14 +3,13 @@ export class HealthScore {
         this.canvas = canvas
         this.context = this.canvas.getContext('2d')
 
-        //this.start = new Date()
         this.score = 10
         this.x = 1100
         this. y = 20
 
     }
     Draw(){
-        //const draw = new Date()
+
         this.context.font = '16px verdana'
         this.context.fillText('Life: ' + this.score, this.x, this.y)
     }
